45cafdac8e6ef426957aa0f3ff3ac9e9caba3f22
felipe ross <kamigamink@gmail.com>
2026-02-09 19:46:21 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		Seller obj = new Seller(1, "felipe", "ala@gmail.com",
 				LocalDate.parse("2006-01-14"), 2000.0, new Department(1,"book"));
 		System.out.println(obj);
+		
+		SellerDao seller = DaoFactory.createSellerDao();
 	}
-
+	
 }
