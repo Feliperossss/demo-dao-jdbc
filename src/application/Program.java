60e54c9b67d16cf4081ac6ec9f711c3e3d559680
felipe ross <kamigamink@gmail.com>
2026-02-09 19:22:32 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "books");
+		Seller obj = new Seller(1, "felipe", "ala@gmail.com",
+				LocalDate.parse("2006-01-14"), 2000.0, new Department(1,"book"));
 		System.out.println(obj);
 	}
 
