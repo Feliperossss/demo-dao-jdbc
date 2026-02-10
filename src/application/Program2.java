50cb32e153aa7fedd05c624d4185474b9173a191
felipe ross <kamigamink@gmail.com>
2026-02-10 20:15:36 -0300

    department findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 
 public class Program2 {
@@ -12,10 +14,18 @@ public class Program2 {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("\n\n=== test 1: seller find by id ====\n\n");
+		System.out.println("\n\n=== test 1: department find by id ====\n\n");
 		System.out.println("enter id for search teste: ");
 		Department department =  departmentDao.findById(2);
 		System.out.println("search complete: " + department);
+		
+		
+		
+		System.out.println("\n\n=== test 2: department find all ====\n\n");
+		List<Department> list = departmentDao.findAll();
+		for(Department obj: list) {
+			System.out.println(obj);
+		}
 		
 		
 		
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -67,6 +68,33 @@ public class DepartmentDaoJDBC  implements DepartmentDao{
 		
 	}
 	
+
+	@Override
+	public List<Department> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			
+			List<Department> list = new ArrayList<>();
+			st = conn.prepareStatement("SELECT department.*\r\n"
+					+ "FROM department\r\n");
+					
+	
+			rs = st.executeQuery();
+			while(rs.next()) {
+				list.add(instantiateDepartment(rs));
+			}
+			
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			
+			DB.closeStatement(st);
+		}
+	}
 	
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
 		
@@ -74,13 +102,6 @@ public class DepartmentDaoJDBC  implements DepartmentDao{
 		dep.setId(rs.getInt("Id"));
 		dep.setName(rs.getString("Name"));
 		return dep;
-	}
-
-
-	@Override
-	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 	
 }
