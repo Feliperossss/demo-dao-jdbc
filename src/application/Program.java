f0193804b46769f43913e4ca57837174d71543ab
felipe ross <kamigamink@gmail.com>
2026-02-10 19:42:36 -0300

    seller delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,9 +13,15 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("\n\n=== test 1: seller find by id ====\n\n");
-		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("enter id for search teste: ");
+		sellerDao.deleteById(sc.nextInt());
+		System.out.println("search complete");
+		
+		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
@@ -35,28 +42,23 @@ public class Program {
 				null,"felipe","felipe@gmail.com", LocalDate.parse("2006-02-23"),2000.0,new Department(2, "Eletronics"));
 		
 		sellerDao.insert(newSeller);
-		
-		List<Seller> list3 = sellerDao.findAll();
-		for(Seller obj: list3) {
-			System.out.println(obj);
-		}
+		System.out.println("insert complete");
 		
 		System.out.println("\n\n=== test 5: seller update ====\n\n");
 		
 		
 		Seller newSeller2 = new Seller(4,"adaWong","ada@gmail.com", LocalDate.parse("1995-02-23"),2000.0,new Department(2, "Eletronics"));
 		sellerDao.update(newSeller2 );
+		System.out.println("update complete");
+	
+		System.out.println("\n\n=== test 6: seller delete  ====\n\n");
+		
+		System.out.println("enter id for delete teste: ");
+		sellerDao.deleteById(sc.nextInt());
+		System.out.println("delete complete");
 		
 		
-		List<Seller> list4 = sellerDao.findAll();
-		for(Seller obj: list4) {
-			System.out.println(obj);
-		}
-		
-		
-		
-		
-		
+		sc.close();
 	}
 
 
